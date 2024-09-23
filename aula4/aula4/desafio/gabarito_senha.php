7361363321b315013853c9048f10ab7d8b1e40bf
LUIZ FREITAS <lafreitas2@yahoo.com.br>
2024-09-23 17:43:49 -0300

    gabarito_senha
    Gabarito do desafio.

--- aula4/aula4/desafio/gabarito_senha.php
+++ aula4/aula4/desafio/gabarito_senha.php
@@ -7,8 +7,20 @@
 </head>
 <body>
 <?php
+    $senha1 = "871960";
     print_r($_POST);
+    $senha_incerida = $_POST['senha'];
     # Seu código aqui.
+    if(isset($senha_incerida)){
+        echo '<br><br>';
+        echo 'Senha: '.$_POST['senha'];
+    }
+    if($senha1!=$senha_incerida){
+        echo '<br><br>';
+        echo 'Você errou!';
+    }
+    else
+        echo 'Acertou!';
 ?>
 </body>
 </html>
