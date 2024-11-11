669f5235493849f6dc8d3ee4c7d6465149c51bfd
LUIZ FREITAS <lafreitas2@yahoo.com.br>
2024-11-11 18:13:58 -0300

    commit

--- aula3_exercicios/001.php
+++ aula3_exercicios/001.php
@@ -6,11 +6,17 @@ Variáveis
 $variavel_1 = 'texto';
 // Imprimir o valor a variável e o seu tipo
 
+echo "Variavel 1: ". $variavel_1. ", Tipo: ". gettype($variavel_1). "<br>";
+
 $variavel_2 = 123;
 // Imprimir o valor a variável e o seu tipo
 
+echo "Variavel 2: ". $variavel_2. ", Tipo: ". gettype($variavel_2). "<br>";
+
 $variavel_3 = 1;
 // Imprimir o valor a variável e o seu tipo
+
+echo "Variavel_3:". $varialvel_3. "Tipo: ". gettype(value: $variavel_3). "<br>";
 
 $array = [
     "foo" => "bar",
@@ -18,4 +24,5 @@ $array = [
 ];
 // Imprimir o valor a variável e o seu tipo
 
+echo "Array: ". print_r($array, true). ", Tipo: ". gettype($array). "<br>";
 ?>
--- aula3_exercicios/002.php
+++ aula3_exercicios/002.php
@@ -6,5 +6,37 @@ Use IF
 $a = 10;
 $b = 1;
 
+if ($a > $b) {
+    echo "O maior número é $a.";
+} else {
+    echo "O maior número é $b.";
+}
+
+// Adicione um novo número
+
+$c = 5;
+
+if ($a > $b && $a > $c) {
+    echo "O maior número é $a.";
+} elseif ($b > $a && $b > $c) {
+    echo "O maior número é $b.";
+} else {
+    echo "O maior número é $c.";
+}
+
+// Adicione mais um novo número
+
+$d = 20;
+
+if ($a > $b && $a > $c && $a > $d) {
+    echo "O maior número é $a.";
+} elseif ($b > $a && $b > $c && $b > $d) {
+    echo "O maior número é $b.";
+} elseif ($c > $a && $c > $b && $c > $d) {
+    echo "O maior número é $c.";
+} else {
+    echo "O maior número é $d.";
+}
+
 
 ?>
